f7144ce6224f615a0ec6618121fc7a4047294199
Elsa Bourgeois <elsa31@gmail.com>
2017-07-10 15:16:36 +0200

    small refactoring

--- spec/PotterSpec.js
+++ spec/PotterSpec.js
@@ -11,7 +11,21 @@ describe("Potter", function() {
   it("two different books should cost 15.20€", function() {
     expect(countNbItemBasket(["tome1","tome2"])).toEqual(15.20);
   });
+
   it("two same books should cost 16€", function() {
     expect(countNbItemBasket(["tome1","tome1"])).toEqual(16);
   });
+
+  it("three same books should cost 24€", function() {
+    expect(countNbItemBasket(["tome1","tome1","tome1"])).toEqual(24);
+  });
+
+  it("3  books, 2 same+1 different should cost 22.8€", function() {
+    expect(countNbItemBasket(["tome1","tome1","tome2"])).toEqual(22.8);
+  });
+
+  // it("3  books, 3 different should cost 21.6€", function() {
+  //   expect(countNbItemBasket(["tome1","tome2","tome3"])).toEqual(21.6);
+  // });
+
 });
--- src/Potter.js
+++ src/Potter.js
@@ -1,21 +1,10 @@
 
 var bookPrice = 8;
 function countNbItemBasket (books) {
-	if (books.length > 1) {
-		if (books[0]!=books[1]) {
-			return books.length*bookPrice*0.95
-		}
-			else {return 16}
-	} else if (books.length ==1) {
-			return books.length*bookPrice
-	} else {
-		return books.length*bookPrice;
-	}
+
+	if ((books.length > 1) && (books[0]!=books[1])) {
+		return books.length*bookPrice*0.95 }
+	else {
+		return books.length*bookPrice}
+
 }
-// function countValueBasket (books) {
-// 	console.log(books[0],books[1]);
-// 	if (books[0]!=books[1]) {
-// 		return 15.20
-// 	}
-// 	else {return 16}
-// }
