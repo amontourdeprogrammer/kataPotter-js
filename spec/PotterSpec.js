3a60b4432587f863fcf384e83e001818627f14f2
Elsa Bourgeois <elsa31@gmail.com>
2017-07-10 12:10:51 +0200

    add test 2 different books

--- spec/PotterSpec.js
+++ spec/PotterSpec.js
@@ -1,10 +1,17 @@
 describe("Potter", function() {
 
   it("one book should cost 8€", function() {
-    expect(countBasket(["tome1"])).toEqual(8);
+    expect(countNbItemBasket(["tome1"])).toEqual(8);
   });
 
   it("no book should cost 0€", function() {
-    expect(countBasket([])).toEqual(0);
+    expect(countNbItemBasket([])).toEqual(0);
+  });
+
+  it("two different books should cost 15.20€", function() {
+    expect(countNbItemBasket(["tome1","tome2"])).toEqual(15.20);
+  });
+  it("two same books should cost 16€", function() {
+    expect(countNbItemBasket(["tome1","tome1"])).toEqual(16);
   });
 });
--- src/Potter.js
+++ src/Potter.js
@@ -1,7 +1,21 @@
-function countBasket (books) {
-	if (books.length != 0) {
-		return 8;
+
+var bookPrice = 8;
+function countNbItemBasket (books) {
+	if (books.length > 1) {
+		if (books[0]!=books[1]) {
+			return books.length*bookPrice*0.95
+		}
+			else {return 16}
+	} else if (books.length ==1) {
+			return books.length*bookPrice
 	} else {
-		return 0;
+		return books.length*bookPrice;
 	}
 }
+// function countValueBasket (books) {
+// 	console.log(books[0],books[1]);
+// 	if (books[0]!=books[1]) {
+// 		return 15.20
+// 	}
+// 	else {return 16}
+// }
